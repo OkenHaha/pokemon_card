75abe1b8a8f6df342b7db197529147cdf0f40bb1
OkenHaha <keithellakpamoken@gmail.com>
2023-02-22 12:55:35 +0530

    testing something out

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,45 @@
 <template>
   <div>
     <h1>List of Pokemon</h1>
-    <button @click="ch()">get url</button>
     <ol>
       <li v-for="pokemon in pokemons" :key="pokemon.name">
         {{ pokemon.name }}
-        <ol><li>
-        {{pokemon.url}}
-      </li>
-      </ol>
+        <ul>
+          <li> {{pokemon.url}} </li>
+        </ul>
+        <br/>
       </li>
     </ol>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 const api = "https://pokeapi.co/api/v2/pokemon"
 const api1 = api + "/1"
 export default {
   data() {
     return {
-      urls: [],
       pokemons: []
     };
   },
   mounted() {
-    // Make an API request to get the data
-    fetch(api + "?offset=0&limit=1300")
-      .then(response => response.json())
-      .then(data => {
-        this.pokemons = data.results;
-        var xy = this.pokemons[0];
-        console.log(xy.url)
-      })
-      .catch(error => console.error(error));
-  },
-  methods: {
-    ch() {
-      fetch(api1)
-      .then(response => response.json())
-      .then(data => {
-        this.urls = data.results
-        console.log(urls)
-      })
-    }
+    axios.get(api + "?limit=2000&offset=0")
+    .then(res => {
+      this.pokemons = res.data.results
+      console.log(this.pokemons[0].name)
+    })
+    
   }
 };
 </script>
+
+
+
+<!-- axios.get(api)
+    .then(res)
+    .then(data => {
+      this.pokemons = data.results
+    })
+    .catch(err => console.error(err) -->)
